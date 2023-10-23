0fd982a15663e86eaf7cb4baba116c99cef12866
Luke Wagener <lukewagener1@gmail.com>
2023-10-23 13:51:44 -0500

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
-end
+    validates :title, presence: true, length: { minimum: 3 }
+  
+    validates :description, presence: true, length: { minimum: 10 }
+  
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  
+    validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  end
+  
