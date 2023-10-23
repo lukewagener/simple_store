a353172e90ed06c9b9d3ecab732ef2d12a2b4f97
Luke Wagener <lukewagener1@gmail.com>
2023-10-23 14:10:17 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'products/index'
   get 'products/action'
   get 'products/view'
+  resources :products, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
