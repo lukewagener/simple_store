776130da40c156308b2fddf864e45a7334b54249
Luke Wagener <lukewagener1@gmail.com>
2023-10-23 14:26:51 -0500

    final

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+    belongs_to :category
     validates :title, presence: true, length: { minimum: 3 }
   
     validates :description, presence: true, length: { minimum: 10 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,22 @@
-require 'faker'
+require 'csv'
 
-# Number of products to seed
-num_products = 676
+Product.destroy_all
+Category.destroy_all
 
-num_products.times do
-  Product.create!(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph(sentence_count: 2), # Faking a description with 2 sentences.
-    price: Faker::Commerce.price(range: 0..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 1000)
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1') # Including encoding just in case
+
+products.each do |product_row|
+  category_name = product_row['category_name'] # Adjust this field name based on your CSV header
+  category = Category.find_or_create_by(name: category_name)
+
+  category.products.create!(
+    title: product_row['title'],
+    description: product_row['description'],
+    price: product_row['price'],
+    stock_quantity: product_row['stock_quantity']
   )
 end
 
-puts "Seeded #{num_products} products!"
+puts "Seeded #{Category.count} categories and #{Product.count} products."
